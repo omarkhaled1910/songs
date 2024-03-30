43863fd05342d49d104fb015d52e7c92c38c27f9
omar <omarkhaled1900@hotmail.com>
2024-03-30 07:19:04 +0200

    fix stale edit problem

--- src/reducers/order.ts
+++ src/reducers/order.ts
@@ -22,12 +22,26 @@ export interface OrderForm {
   filteredSongs: { songs: TSong[]; albumName: string }[];
   userInfo: UserData;
 }
-const handleToggle = (arr: number[], newId: number) => {
-  const newArray = arr.includes(newId)
+const handleToggle = (arr: number[], newId: number, isExist: boolean) => {
+  const newArray = isExist
     ? arr.filter((albumId) => albumId !== newId)
     : [...arr, newId];
 
   return newArray;
+};
+const getNewlySelectedSongsAfterSingerUnSelect = (
+  prevSongsIds: number[],
+  avialableSongs: any[]
+) => {
+  const newSelectedSongs: number[] = [];
+  prevSongsIds.map((prevSelectedSong) =>
+    avialableSongs.find((newSong) =>
+      newSong.songs.find((song: any) => prevSelectedSong === song.id)
+    )
+      ? newSelectedSongs.push(prevSelectedSong)
+      : undefined
+  );
+  return newSelectedSongs;
 };
 
 const initialState: OrderForm = {
@@ -49,21 +63,86 @@ export const OrderFormSlice = createSlice({
   initialState,
   reducers: {
     setSingersIds: (state, action: PayloadAction<number>) => {
-      const newSingersIds = handleToggle(state.singerIds, action.payload);
+      const isExists = state.singerIds.includes(action.payload);
+
+      const newSingersIds = handleToggle(
+        state.singerIds,
+        action.payload,
+        isExists
+      );
+      const updatedAlbums = getAlbumsBySingerIds(newSingersIds);
 
       state.singerIds = newSingersIds;
-      state.filteredAlbums = getAlbumsBySingerIds(newSingersIds);
-      state.price = calculatePrice({ ...state, singerIds: newSingersIds });
+      state.filteredAlbums = updatedAlbums;
+      //handle side effects and remove previsoly selected albums and songs
+
+      if (isExists) {
+        const newSelectedAlbums: number[] = [];
+        state.albumIds.forEach((album) =>
+          updatedAlbums.find((newAlbum) => newAlbum.id === album)
+            ? newSelectedAlbums.push(album)
+            : undefined
+        );
+
+        state.albumIds = newSelectedAlbums;
+        const updatedSongs = getSongsByAlbumsIds(newSelectedAlbums);
+
+        const newSelectedSongs = getNewlySelectedSongsAfterSingerUnSelect(
+          [...state.songsIds],
+          updatedSongs
+        );
+        state.price = calculatePrice({
+          ...state,
+          singerIds: newSingersIds,
+          albumIds: newSelectedAlbums,
+          songsIds: newSelectedSongs,
+        });
+        state.songsIds = newSelectedSongs;
+        return;
+      }
+
+      state.price = calculatePrice({
+        ...state,
+        singerIds: newSingersIds,
+      });
     },
     setAlbumsIds: (state, action: PayloadAction<number>) => {
-      const newAlbumsIds = handleToggle(state.albumIds, action.payload);
-
+      const isExists = state.albumIds.includes(action.payload);
+      const newAlbumsIds = handleToggle(
+        state.albumIds,
+        action.payload,
+        isExists
+      );
+      const updatedSongs = getSongsByAlbumsIds(newAlbumsIds);
       state.albumIds = newAlbumsIds;
-      state.filteredSongs = getSongsByAlbumsIds(newAlbumsIds);
-      state.price = calculatePrice({ ...state, albumIds: newAlbumsIds });
+      state.filteredSongs = updatedSongs;
+      let newSelectedSongs: number[] = [];
+      if (isExists) {
+        newSelectedSongs = getNewlySelectedSongsAfterSingerUnSelect(
+          [...state.songsIds],
+          updatedSongs
+        );
+        state.songsIds = newSelectedSongs;
+        state.price = calculatePrice({
+          ...state,
+          albumIds: newAlbumsIds,
+          songsIds: newSelectedSongs,
+        });
+        return;
+      }
+      state.price = calculatePrice({
+        ...state,
+        albumIds: newAlbumsIds,
+      });
     },
     setSongsIds: (state, action: PayloadAction<number>) => {
-      const newSongsIds = handleToggle(state.songsIds, action.payload);
+      const isExists = state.songsIds.includes(action.payload);
+
+      const newSongsIds = handleToggle(
+        state.songsIds,
+        action.payload,
+        isExists
+      );
       state.songsIds = newSongsIds;
       state.price = calculatePrice({ ...state, songsIds: newSongsIds });
     },
